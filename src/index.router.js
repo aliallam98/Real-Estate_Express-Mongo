35b4ffe39c32347d038590c67486accd656ee759
aliallam98 <allam98.ali@gmail.com>
2023-12-08 04:18:07 +0200

    Build src folder and prepared Validations,Routers,Controllers Files

--- src/index.router.js
+++ src/index.router.js
@@ -5,13 +5,18 @@
     app.use(express.json())
     app.use(cors())
 
+    app.use("/api/user",userRouter)
+    app.use("/api/category",categoryRouter)
+    app.use("/api/listing",listingRouter)
 
 
+    //Not Found Routes
     app.all('*', (req, res, next) => {
         res.send("In-valid Routing Please Check Url Or Method")
     })
 
 
+    //G-Err Handler
     app.use((error,req,res,next)=>{
         return res.status(error.status || 400).json({ message: error.message, stack : error.stack })
     })
